09ea7b8928f43dcb4f0112d2b2334eeb5947eb67
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-03 18:14:44 -0300

    Initial Commit

---
+++ WeatherSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSwift
+//
+//  Created by Luciano dii Souza on 03/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSwift/WeatherSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftApp.swift
+//  WeatherSwift
+//
+//  Created by Luciano dii Souza on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
