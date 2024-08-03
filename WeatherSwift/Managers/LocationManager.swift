40c7199d3933f81e58b68d55cdba69cf2c0ebeaa
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-03 18:24:51 -0300

    creating LocationManager

---
+++ WeatherSwift/Managers/LocationManager.swift
@@ -0,0 +1,35 @@
+//
+//  LocationManager.swift
+//  WeatherSwift
+//
+//  Created by Luciano dii Souza on 03/08/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Error getting location", error )
+    }
+}
