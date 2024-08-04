3108fb8148e93f46983f8a28b396daed951dd31b
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-04 19:39:24 -0300

    wip WeatherView

--- WeatherSwift/Views/WeatherView.swift
+++ WeatherSwift/Views/WeatherView.swift
@@ -11,7 +11,34 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name).bold().font(.title)
+                    
+                    Text("Today \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text(weather.main.feelsLike.roundDouble() + "º")
+                            .font(.system(size: 100))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                    }
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.765, saturation: 0.948, brightness: 0.544))
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
