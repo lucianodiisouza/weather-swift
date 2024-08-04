c26b94353060ca7656c67e9669b605e0a8b8ca75
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-04 12:32:07 -0300

    fetching weather data (api key obfuscated)

---
+++ WeatherSwift/Managers/WeatherManager.swift
@@ -0,0 +1,63 @@
+//
+//  WeatherManager.swift
+//  WeatherSwift
+//
+//  Created by Luciano dii Souza on 03/08/24.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees ) async throws {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(longitude)&lon=\(longitude)&appid={APIKEYHERE}&units=metric") else { fatalError("URL is missing...")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data...")}
+
+    }
+}
+
+// Model of the response body we get from calling the OpenWeather API
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
+}
