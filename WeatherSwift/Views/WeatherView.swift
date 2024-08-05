f198a8536434e7a57f2e4a5b85db0ef93ab57c72
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-04 21:31:24 -0300

    wip weather view

--- WeatherSwift/Views/WeatherView.swift
+++ WeatherSwift/Views/WeatherView.swift
@@ -25,16 +25,53 @@ struct WeatherView: View {
                 
                 VStack {
                     HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            Text(weather.weather[0].main)
+                        }
+                            .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
                         Text(weather.main.feelsLike.roundDouble() + "º")
                             .font(.system(size: 100))
                             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                             .padding()
                     }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) {
+                        image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        LoadingView()
+                    }
+                    
+                    Spacer()
                 }
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.765, saturation: 0.948, brightness: 0.544))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.765, saturation: 0.948, brightness: 0.544))
